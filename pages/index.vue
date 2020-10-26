905c099692403e6a680b51746f528dc851fbdcc7
peterugah <p.ugah.pu@gmail.com>
2020-10-26 22:57:55 +0100

    testing env variale setup

---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="columns is-mobile">
+    <div class="column is-6 has-text-centered">{{ ADMIN_END_ROUTE }}</div>
+    <div class="column is-6 has-text-centered">hello</div>
+  </div>
+</template>
+
+<script>
+export default {
+  asyncData({ $config }) {
+    return {
+      ADMIN_END_ROUTE: $config.ADMIN_END_ROUTE,
+    }
+  },
+}
+</script>
